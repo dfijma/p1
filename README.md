230e422750eaa8c8b1a812141d2274b0ff5462b0
Duco Fijma <duco@fijma.net>
2021-04-17 20:06:23 +0200

    README

---
+++ README.md
@@ -0,0 +1,8 @@
+# P1
+
+Effe lekker prutsen:
+- inlezen P1 gegevens van meter via seriële poort
+- CRC checks geïmplementeerd, maar (nog) niet gehandhaafd
+- parsen van interessante getalletjes uit de telegrams
+- inlezen van status van Omnik omvormer via het "/js/status.js"-achterdeurtje
+- doorsturen interessante getallen via UDP-broadcast
--- p1.py
+++ p1.py
@@ -4,13 +4,13 @@ import socket
 import sys
 import serial
 from urllib import request
-from datetime import datetime
 import time
 
-##### parse p1 lines
+## parse p1 lines
 
 c = 0
 input = []
+
 
 def cur():
     if c < len(input):
@@ -18,9 +18,11 @@ def cur():
     else:
         return chr(0)
 
+
 def skip():
     global c
     c = min(c + 1, len(input))
+
 
 def parse_c(unit_allowed):
     global c
@@ -43,6 +45,7 @@ def parse_c(unit_allowed):
             raise ValueError("unit not allowed")
         return input[start:end_val]
 
+
 def parse_bracket_c():
     if cur() != '(':
         raise ValueError("( expected")
@@ -53,53 +56,58 @@ def parse_bracket_c():
     skip()
     return val
 
+
 def parse_bracket_list():
     f = parse_bracket_c()
     if cur() == chr(0):
         return [f]
     else:
         rest = parse_bracket_list()
-        rest.insert(0,f)
+        rest.insert(0, f)
         return rest
+
 
 def parse(s):
     global c
     global input
     input = s
     c = 0
-    name  = parse_c(False)
+    name = parse_c(False)
     list = parse_bracket_list()
     # print(name)
     # print(list)
     return (name, list)
+
 
 def parse_lines(lines_read):
     while True:
         for lines in lines_read:
             for line in lines:
                 try:
-                    yield(parse(line))
+                    yield (parse(line))
                 except ValueError:
                     pass
+
 
 ##### serial p1 communication
 
 ser = serial.Serial()
 ser.baudrate = 115200
-ser.bytesize=serial.EIGHTBITS
-ser.parity=serial.PARITY_NONE
-ser.stopbits=serial.STOPBITS_ONE
-ser.xonxoff=0
-ser.rtscts=0
-ser.timeout=20
-ser.port="/dev/ttyUSB0"
+ser.bytesize = serial.EIGHTBITS
+ser.parity = serial.PARITY_NONE
+ser.stopbits = serial.STOPBITS_ONE
+ser.xonxoff = 0
+ser.rtscts = 0
+ser.timeout = 20
+ser.port = "/dev/ttyUSB0"
 
 try:
     ser.open()
 except:
     sys.exit(f"error opening serial port {ser.name}")
 
-crc=0
+crc = 0
+
 
 def calc_crc_telegram(telegram):
     global crc
@@ -108,9 +116,10 @@ def calc_crc_telegram(telegram):
         for y in range(8):
             if (crc & 1) != 0:
                 crc = crc >> 1
-                crc = crc ^ (int("0xA001",16))
+                crc = crc ^ (int("0xA001", 16))
             else:
                 crc = crc >> 1
+
 
 def read_p1():
     state = 0
@@ -120,7 +129,7 @@ def read_p1():
             p1_raw = ser.readline()
         except:
             sys.exit(f"cannot read serial port {ser.name}")
-        p1_str=p1_raw.decode('ascii')
+        p1_str = p1_raw.decode('ascii')
         p1_line = p1_str.strip()
         # print(p1_raw)
         if p1_line.startswith("!"):
@@ -132,15 +141,15 @@ def read_p1():
                 crc = 0
                 yield lines
             else:
-               # unhappy, reset
-               # print("reset, unexpected crc line")
-               state = 0
-               crc = 0
+                # unhappy, reset
+                # print("reset, unexpected crc line")
+                state = 0
+                crc = 0
         else:
             calc_crc_telegram(p1_raw)
             if p1_line == "":
                 if state == 1:
-                    #happy: data starts after this
+                    # happy: data starts after this
                     state = 2
                 else:
                     # unhappy: reset
@@ -154,16 +163,18 @@ def read_p1():
             else:
                 if state == 2:
                     # happy, data line
-                    lines.append(p1_line) 
+                    lines.append(p1_line)
                 else:
                     # print("reset, unexpected data line")
                     state = 0
                     crc = 0
 
+
 ##### read-out interesting values from parsed p1 lines 
 
 names = ['1-0:1.7.0', '1-0:2.7.0']
 vals = {}
+
 
 def readouts():
     global vals
@@ -173,18 +184,20 @@ def readouts():
     for (name, values) in parse_lines(lines_read):
         if name in names:
             if len(values) == 1:
-               value = values[0]
-               try:
-                   fvalue = float(value[0])
-                   vals[name] = fvalue
-               except ValueError:
-                   pass
-        yield(vals)
+                value = values[0]
+                try:
+                    fvalue = float(value[0])
+                    vals[name] = fvalue
+                except ValueError:
+                    pass
+        yield (vals)
+
 
 ##### omnik
 
 # yuck, but works: https://github.com/Woutrrr/Omnik-Data-Logger/issues/27
 url = "http://10.1.0.104/js/status.js"
+
 
 def read_omnik():
     while True:
@@ -194,26 +207,27 @@ def read_omnik():
                 if "myDeviceArray[0]=" in d:
                     ps = d.split(',')
                     [now, today, total] = ps[5:8]
-                    now = float(now) / 1000  
+                    now = float(now) / 1000
                     today = float(today) / 100  # 1/100's of kWh
-                    total = float(total) / 10  # convert 1/10's to 1/100; of kWh
-                    yield(now, today, total)
+                    total = float(total) / 10  # c1/10's of kWh
+                    yield now, today, total
                     break
         except Exception as inst:
             print("error: ", inst)
-            
+
+
 ##### main
 
-client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # UDP
+client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # UDP
 client.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
 
 for t in zip(readouts(), read_omnik()):
-   ps = t[1][0]
-   pr = t[0].get('1-0:2.7.0')
-   pd = t[0].get('1-0:1.7.0')
-   print(t)
-   if pr != None  and pd != None:
-       pl = pd - pr
-       msg=f"pl={pl} ps={ps}"
-       client.sendto(msg.encode('UTF-8'), ('<broadcast>', 37020))
-   time.sleep(1)
+    ps = t[1][0]
+    pr = t[0].get('1-0:2.7.0')
+    pd = t[0].get('1-0:1.7.0')
+    print(t)
+    if pr is not None and pd is not None:
+        pl = pd - pr
+        msg = f"pl={pl} ps={ps}"
+        client.sendto(msg.encode('UTF-8'), ('<broadcast>', 37020))
+    time.sleep(1)
